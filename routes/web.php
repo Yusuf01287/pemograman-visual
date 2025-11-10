8bcb692d3377ff65405e058d94f68a5320d19fc3
Yusuf0128 <latiflatif070707@gmail.com>
2025-11-10 20:31:12 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/template', function() {
+    return view ('template');
+});
 Route::get('/', function () {
-    return view('welcome');
+    return view ('admin.layouts.wrapper');
 });
